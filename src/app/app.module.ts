8ab595c3ad7b427b1ff3a5551744e249669c1189
Maja Nordhild <majanord@gmail.com>
2021-06-14 01:09:49 +0200

    from test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,56 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { ShortListComponent } from './short-list/short-list.component';
+import { HttpClientModule } from "@angular/common/http";
+import { DetailsComponent } from './details/details.component';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule } from "@angular/material/radio";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { ResultsComponent } from './results/results.component';
+import { MatListModule } from "@angular/material/list";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ShortListComponent,
+    DetailsComponent,
+    SearchComponent,
+    ResultsComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatInputModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
